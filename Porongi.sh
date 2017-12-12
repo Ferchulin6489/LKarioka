f031dc0d3f91240aec680dea7f404a68dc06fa27
Ferchulin6489 <Ferchulin_64_89@hotmail.com>
2017-12-12 19:18:39 +0100

    Update Porongi.sh

--- Porongi.sh
+++ Porongi.sh
@@ -2,11 +2,11 @@
 
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:george-edison55/cmake-3.x
-sudo apt-get update
+sudo apt-get update -y
 
 sudo apt-get install -y cmake
 
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 sudo apt-get install -y build-essential cmake libuv1-dev libmicrohttpd-dev
 git clone https://github.com/xmrig/xmrig.git
