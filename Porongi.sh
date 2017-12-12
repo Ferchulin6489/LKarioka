ee1534cb73f6afc4cf218b8bc2d897f74f52c83a
DevLock <Ferchulin6489@Github.com>
2017-12-12 19:15:16 +0100

    a

---
+++ Porongi.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:george-edison55/cmake-3.x
+sudo apt-get update
+
+sudo apt-get install -y cmake
+
+sudo apt-get upgrade
+
+sudo apt-get install -y build-essential cmake libuv1-dev libmicrohttpd-dev
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
